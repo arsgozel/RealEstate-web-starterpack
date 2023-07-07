b685094b883f847429c0c96e3e70a4370633ad1a
Gozel <30gozel30gozel@gmail.com>
2023-07-07 17:06:35 +0500

    1.1.0
    Header, Hero, Companies are completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Companies from './components/Companies/Companies'
 
+import './App.css'
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div className="">
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+    </div>
   );
 }
 
